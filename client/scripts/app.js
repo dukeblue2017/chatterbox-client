388a461f4b735769f1d8a7144ad4600b91f85e93
Hack Reactor Student <student@hackreactor.com>
2017-10-13 16:11:06 -0500

    passing tests up through fetching

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,46 @@
-// YOUR CODE HERE:
+class App { 
+  
+  constructor() {
+    this.server = 'http://parse.atx.hackreactor.com/chatterbox/classes/messages';
+  }
+
+  init() {
+    return 'hello';
+  }
+  
+  send(message) {
+    $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+      url: this.server,
+      type: 'POST',
+      data: message,
+      /*contentType: 'application/json',*/
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message', data);
+      }
+    });  
+  }
+  
+  fetch() {
+    
+    $.ajax({
+      url: this.server,
+      type: 'GET',
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: "That\'s so fetch"');
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Quit trying to make "Fetch" happen, Gretchen');
+      }
+    });
+  }
+}
+
+
+var app = new App();
